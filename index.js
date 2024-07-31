4a925aef362a778331892d6df0bf4ef982d75d1d
DavSan <david.sanchez.martinez@outlook.com>
2024-07-31 18:56:07 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 function addTask() {
     const taskInput = document.getElementById('taskInput'); // Retrieve the value from the input field
     const taskText = taskInput.value.trim(); // Get the value and trim any whitespace
-
+    // Check if the Input is Not Empty
+    if(taskText !== '') {
+        
+    }
      
 };
