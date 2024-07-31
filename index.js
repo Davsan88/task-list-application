2b0324c23db124961ca7cf919b380e1488353db4
DavSan <david.sanchez.martinez@outlook.com>
2024-07-31 19:51:14 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
+// Get references to the input field, button, and task list
+const taskInput = document.getElementById('taskInput');
+const taskList = document.getElementById('taskList');
+const taskButton = document.getElementById('addTaskButton');
+
 function addTask() {
-    const taskInput = document.getElementById('taskInput'); // Retrieve the value from the input field
     const taskText = taskInput.value.trim(); // Get the value and trim any whitespace
     // Check if the Input is Not Empty
     if(taskText !== '') {
          // Proceed with creating the new task
-        const listElement = document.createElement('li'); // Create a new <li> element
-        listElement.textContent = taskText; // Set the text content to the input value
-        const taskList = document.getElementById('taskList');
-        taskList.appendChild('listElement'); // Append the new task to the task list
+        const newTask = document.createElement('li'); // Create a new <li> element
+        newTask.textContent = taskText; // Set the text content to the input value
+        newTask.classList.add('task'); // Add the 'task' class for styling
+
+        taskList.appendChild('newTask'); // Append the new task to the task list
+
+        taskInput.value = ''; // Clear the input field
     }
      
 };
