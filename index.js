abb3d734cffa18dfcd954bc1d17f63371fba826d
DavSan <david.sanchez.martinez@outlook.com>
2024-07-31 18:49:57 +0100

    Update index.js

---
+++ index.js
@@ -0,0 +1,6 @@
+function addTask() {
+    const taskInput = document.getElementById('taskInput'); // Retrieve the value from the input field
+    const taskText = taskInput.value.trim(); // Get the value and trim any whitespace
+
+     
+};
