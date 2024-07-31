1a1844cf46f8b8869ea1feccc69e008fa741dba3
DavSan <david.sanchez.martinez@outlook.com>
2024-07-31 19:22:35 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,11 @@ function addTask() {
     const taskText = taskInput.value.trim(); // Get the value and trim any whitespace
     // Check if the Input is Not Empty
     if(taskText !== '') {
-        
+         // Proceed with creating the new task
+        const listElement = document.createElement('li'); // Create a new <li> element
+        listElement.textContent = taskText; // Set the text content to the input value
+        const taskList = document.getElementById('taskList');
+        taskList.append('listElement'); // Append the new task to the task list
     }
      
 };
